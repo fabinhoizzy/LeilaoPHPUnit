e3e05cc8181ff3624948831ccf069179eb354279
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-05-25 19:02:36 -0300

    Adicionando Mock no testes

---
+++ tests/Service/EncerradorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Alura\Leilao\Tests\Service;
+
+use Alura\Leilao\Dao\Leilao as LeilaoDao;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Service\Encerrador;
+use PHPUnit\Framework\TestCase;
+
+
+class EncerradorTest extends TestCase
+{
+    public function testLeiloesComMaisDeUmaSemanaDevemSerEncerrados()
+    {
+        $fiat147 = new Leilao(
+            'Fiat 147 0km',
+            new \DateTimeImmutable('8 days ago')
+        );
+        $variant = new Leilao(
+            'Variant 1972 0km',
+            new \DateTimeImmutable('10 days ago')
+        );
+        $leilaoDao = $this->createMock(LeilaoDao::class);
+
+        $leilaoDao->method('recuperarNaoFinalizados')->willReturn([$fiat147, $variant]);
+        $leilaoDao->method('recuperarFinalizados')->willReturn([$fiat147, $variant]);
+
+        $encerrador = new Encerrador($leilaoDao);
+        $encerrador->encerra();
+
+        $leiloes = [$fiat147, $variant];
+        self::assertCount(2, $leiloes);
+        self::assertTrue( $leiloes[0]->estaFinalizado());
+        self::assertTrue($leiloes[1]->estaFinalizado());
+    }
+}
